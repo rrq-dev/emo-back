b45d5eaf8389496e6cfd61c3c42c509e63e9638a
rrq-dev <raihanhermawan85@gmail.com>
2025-06-22 14:04:07 +0700

    CODE: memperbaiki cors (error dibagian header wak gara gara "/" )

--- config/cors.go
+++ config/cors.go
@@ -1,8 +1,8 @@
 package config
 
 var AllowedOrigins = []string{
-	"https://emo-back.onrender.com/",
-	"https://emobuddy-495ef.web.app/",
+	"https://emo-back.onrender.com",
+	"https://emobuddy-495ef.web.app",
 }
 
 var GetAllowedOrigins = func() []string {
