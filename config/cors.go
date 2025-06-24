b6dcc5251589fea6d2206823c70041166cda0c5e
rrq-dev <raihanhermawan85@gmail.com>
2025-06-24 19:06:15 +0700

    DOCS: kita mulai dari sini (error mulu njer)

--- config/cors.go
+++ config/cors.go
@@ -2,7 +2,7 @@ package config
 
 var AllowedOrigins = []string{
 	"https://emo-back.onrender.com",
-	"https://emobuddy-495ef.web.app",
+	"https://emobuddy-495ef.web.app/",
 }
 
 var GetAllowedOrigins = func() []string {
