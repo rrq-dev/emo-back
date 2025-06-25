6d6ca2a2a5782f6408df5ffb4a2f5131dd390f90
rrq-dev <raihanhermawan85@gmail.com>
2025-06-26 00:09:05 +0700

    CODE: refactor clean up comments in route setup for clarity

--- routes/route.go
+++ routes/route.go
@@ -15,14 +15,14 @@ func SetupURL(app *fiber.App) {
 	api.Post("/login", ctrl.Login)
 	api.Post("/register", ctrl.Register)
 
-		// ✅ Routes yang bisa anonymous atau login
+		// Routes yang bisa anonymous atau login
 	api.Post("/moods", middleware.JWTOptional(), control.SubmitMoods)
 	api.Get("/moods", control.GetAllMoodsData) // Public route
+		// Gemini route
+	api.Get("/reflection/latest", control.GetAllChatReflections)
+	api.Post("/reflection", control.PostChatReflection)
 
-	 // ✅ Routes yang HARUS login
-	// api.Get("/moods/:userId", middleware.JWTProtected(), control.GetUserMoods)
-	// api.Get("/chat-reflections", middleware.JWTProtected(), control.GetLatestReflection)
-	// api.Post("/chat-refleksi", middleware.JWTProtected(), control.SubmitChatReflection)
+
 }
 
 
